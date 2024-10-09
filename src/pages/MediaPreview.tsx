f49e8a05d7ca33d2125583f2b451a64de7cab1cd
itseasy21 <itseasy21@gmail.com>
2024-10-09 13:28:18 +1100

    preload hero image

--- src/pages/MediaPreview.tsx
+++ src/pages/MediaPreview.tsx
@@ -346,7 +346,15 @@ export function MediaPreview() {
         <meta property="og:url" content={window.location.href} />
         <link rel="canonical" href={window.location.href} />
         <script type="application/ld+json">{JSON.stringify(schemaData)}</script>
-        {heroImageUrl && <link rel="preload" as="image" href={heroImageUrl} />}
+        {heroImageUrl && (
+          <link
+            rel="preload"
+            as="image"
+            href={heroImageUrl}
+            // eslint-disable-next-line react/no-unknown-property
+            fetchPriority="high"
+          />
+        )}
         <style type="text/css">{`
           html, body {
             scrollbar-width: none;
